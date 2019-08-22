c728a1f6e15daf5931d790640ebeafb9d88f0b75
Darryl Kilzer <darrylkilzer@gmail.com>
2019-08-22 15:03:07 -0600

    adds the localstorage

--- app/Services/PizzaService.js
+++ app/Services/PizzaService.js
@@ -1,33 +1,48 @@
 import Pizza from "../Models/Pizza.js";
 
 let _state = {
-  pizzas: [new Pizza({
-    name: "Pepperoni",
-    size: "XL",
-    toppings: ["cheese", "sauce", "pepperoni"],
-    crust: "pan",
-    price: 2
-  })]
+  pizzas: []
 }
 
 export default class PizzaService {
   deleteTopping(pizzaIndex, toppingIndex) {
     _state.pizzas[pizzaIndex].toppings.splice(toppingIndex, 1)
+    this.savePizzas()
   }
   deletePizza(index) {
     _state.pizzas.splice(index, 1)
+    this.savePizzas()
   }
   addTopping(newTopping, pizzaIndex) {
     _state.pizzas[pizzaIndex].toppings.push(newTopping)
+    this.savePizzas()
   }
   addPizza(newPizza) {
     _state.pizzas.push(new Pizza(newPizza))
+    this.savePizzas()
     console.log(_state.pizzas)
   }
   constructor() {
     console.log("Howdy from pizza service........")
+    this.loadPizzas()
+
   }
   get Pizza() {
     return _state.pizzas.map(pizza => new Pizza(pizza))
   }
+
+
+  loadPizzas() {
+    let savedPizzas = JSON.parse(localStorage.getItem("pizzas"))
+    if (savedPizzas) {
+      _state.pizzas = savedPizzas
+    }
+  }
+
+  savePizzas() {
+    console.log(JSON.stringify(_state.pizzas))
+    localStorage.setItem("pizzas", JSON.stringify(_state.pizzas))
+  }
+
+
 }
